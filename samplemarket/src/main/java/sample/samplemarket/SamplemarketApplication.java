ae4be42a3c082335ee3babf3b7ef363b43cd4b9b
Hyun Ho <hhjeong@gmail.com>
2023-03-03 10:13:26 +0900

    프로젝트 생성

---
+++ samplemarket/src/main/java/sample/samplemarket/SamplemarketApplication.java
@@ -0,0 +1,13 @@
+package sample.samplemarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamplemarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamplemarketApplication.class, args);
+	}
+
+}
